e21813b8be2290a8486d50f25bd2b8215991b6b1
mjLamarree <lamarre.m@yahoo.com>
2023-05-21 07:55:16 -0400

    Project Setup - Tailwind, Vite, TS
    Setup the file and some of the first libs i'll be using, TailindCSS, and typescript. Project built using vite

---
+++ Toto-Team-Build/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className='w-full h-full flex justify-center'>
+        <span className='text-red-400 underline text-3xl'>
+            Test 123 for Tailwind
+          </span>
+      </div>
+    </>
+  )
+}
+
+export default App
